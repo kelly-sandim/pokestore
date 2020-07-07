b09b37bb8db5a1c2732be94ee20c62795955a413
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-06 20:30:26 -0400

    Updated the homepage buttons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Col } from 'reactstrap';
+import { Col } from 'reactstrap';
 import { Button } from 'reactstrap';
 import PokeSteel from '../../assets/pokestore-steel.svg';
 import PokeGrass from '../../assets/pokestore-grass.svg';
@@ -9,17 +9,15 @@ import PokeGrass from '../../assets/pokestore-grass.svg';
 function Home() {
       
   return (
-    <>
-      <Container fluid="lg" >
-        <Col>
-            <Link to="/steel">
-                <Button><img src={PokeSteel} alt=""/></Button>
-            </Link>
-            <Link to="/grass">
-                <Button><img src={PokeGrass} alt=""/></Button>
-            </Link>
-        </Col>
-      </Container>
+    <>      
+      <Col>
+          <Link to="/steel">
+              <Button id="steel-button"><img src={PokeSteel} alt=""/></Button>
+          </Link>
+          <Link to="/grass">
+              <Button color="success" id="grass-button"><img src={PokeGrass} alt=""/></Button>
+          </Link>
+      </Col>      
     </>
   );
 }

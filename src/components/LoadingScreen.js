8c1fdb7baa90366eb5174bb9f876fa9fa0581e9e
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-07-10 21:18:03 -0400

    Added the component of loading screen, but it isn't called yet

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingScreen = props => {
+    return(        
+        <div id="loader" className="loaderCenter"></div>  
+    );
+};
+
+export default LoadingScreen;
